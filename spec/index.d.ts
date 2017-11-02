0b2d475e7072c94eb96988dc299fe5d90d78a6b5
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2017-11-02 16:07:26 -0400

    Update export names

--- spec/index.d.ts
+++ spec/index.d.ts
@@ -1,10 +1,10 @@
 // RESTyped implementations should take a type parameter that
 // extends Base
-export interface RestypeBase {
+export interface RestypedBase {
   [route: string]: any
 }
 
-export interface RestypeRoute {
+export interface RestypedRoute {
   params: any
   query: any
   body: any
@@ -14,11 +14,11 @@ export interface RestypeRoute {
 // Here for reference. It's not recommended to extend your API
 // definition from IndexedBase, because then your definition will
 // not cause errors when an invalid route is defined or called
-export interface RestypeIndexedBase {
+export interface RestypedIndexedBase {
   // e.g. '/orders'
   [route: string]: {
     // 'GET' | 'POST' | 'PUT' | 'PATCH' | 'HEAD' | 'DELETE' | 'OPTIONS'
-    [method: string]: RestypeRoute
+    [method: string]: RestypedRoute
   }
 }
 
